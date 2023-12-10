3f3efdc7ce23adcdb6005393351c4b024b042390
Ogochukwu Ozotta <88560609+ogochukwu-ozotta@users.noreply.github.com>
2023-12-10 05:42:57 -0600

    Add widgets

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+import streamlit as st
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+st.title("Weather Forecast for the Next Days")
+place = st.text_input("Place: ")
+days = st.slider("Forecast Days", min_value=1, max_value=5,
+                 help="Select the number of forecasted days")
+option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+st.subheader(f"{option} for the next {days} days in {place}")
